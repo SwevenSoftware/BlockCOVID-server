68bfbef613e36778451d0bef20ce9e3b74e34e0e
nR3D <nR3D@pm.me>
2021-02-24 20:05:13 +0100

    Optional reservation repository

--- src/main/java/it/sweven/blockcovid/repositories/ReservationRepository.java
+++ src/main/java/it/sweven/blockcovid/repositories/ReservationRepository.java
@@ -1,18 +1,19 @@
 package it.sweven.blockcovid.repositories;
 
 /* Java utilities */
+
+import it.sweven.blockcovid.entities.Reservation;
 import java.time.LocalDate;
 import java.util.List;
-
-/* Spring imports */
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-/* Our imports */
-import it.sweven.blockcovid.entities.Reservation;
-
 public interface ReservationRepository extends MongoRepository<Reservation, String> {
-    List<Reservation> findAllByUser(String user);
-    Reservation findByIdAndUser(String id, String user);
-    Reservation deleteByIdAndUser(String id, String user);
-    List<Reservation> findByNameRoomAndDate(String nameRoom, LocalDate date);
+  Optional<List<Reservation>> findAllByUser(String user);
+
+  Optional<Reservation> findByIdAndUser(String id, String user);
+
+  Optional<Reservation> deleteByIdAndUser(String id, String user);
+
+  Optional<List<Reservation>> findByNameRoomAndDate(String nameRoom, LocalDate date);
 }
