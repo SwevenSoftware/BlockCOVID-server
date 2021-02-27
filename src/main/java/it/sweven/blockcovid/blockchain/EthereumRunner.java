4d9919ae799e5276090b74c8f0a7af7b3ebe3e94
Luca Zaninotto <lucazanny@gmail.com>
2021-02-27 23:46:31 +0100

    rimosse cose non necessarie

--- src/main/java/it/sweven/blockcovid/blockchain/EthereumRunner.java
+++ src/main/java/it/sweven/blockcovid/blockchain/EthereumRunner.java
@@ -21,24 +21,10 @@ import org.web3j.tx.gas.DefaultGasProvider;
 @Component
 public class EthereumRunner {
 
-  @Autowired private final UserRepository userRepository;
   @Autowired private final ReservationRepository reservationRepository;
 
   private Web3j connection;
   private EthereumConfiguration configuration;
-
-  private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-  // abominio trovato online
-  public static String bytesToHex(byte[] bytes) {
-    char[] hexChars = new char[bytes.length * 2];
-    for (int j = 0; j < bytes.length; j++) {
-      int v = bytes[j] & 0xFF;
-      hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-      hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-    }
-    return new String(hexChars);
-  }
 
   EthereumRunner(UserRepository userRepository, ReservationRepository reservationRepository) {
     this.userRepository = userRepository;
