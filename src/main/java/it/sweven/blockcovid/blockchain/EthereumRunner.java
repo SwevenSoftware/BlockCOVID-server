86ed64cae6d004cd932ffc1f1fc70598f475653d
Luca Zaninotto <lucazanny@gmail.com>
2021-03-04 16:04:37 +0100

    rimosse linee di debug

--- src/main/java/it/sweven/blockcovid/blockchain/EthereumRunner.java
+++ src/main/java/it/sweven/blockcovid/blockchain/EthereumRunner.java
@@ -31,11 +31,6 @@ public class EthereumRunner {
 
     configuration = new EthereumConfiguration();
 
-    System.out.println("==================================");
-    System.out.println("==================================");
-    System.out.println("NETWORK = " + configuration.NETWORK);
-    System.out.println("==================================");
-    System.out.println("==================================");
     connection = Web3j.build(new HttpService(configuration.NETWORK));
     /*
      * Se non è stato indicato un indirizzo del contratto, fare il deploy con
