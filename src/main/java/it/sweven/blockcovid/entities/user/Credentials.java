4cd5db77582f289d54220d2412b98b1c7b3689ea
nR3D <nR3D@pm.me>
2021-03-12 22:04:50 +0100

    spotless Credentials

--- src/main/java/it/sweven/blockcovid/entities/user/Credentials.java
+++ src/main/java/it/sweven/blockcovid/entities/user/Credentials.java
@@ -1,40 +1,39 @@
 package it.sweven.blockcovid.entities.user;
 
 import it.sweven.blockcovid.security.Authority;
-
 import java.util.Set;
 
 public class Credentials {
-    private String username, password;
-    private Set<Authority> authorities;
+  private String username, password;
+  private Set<Authority> authorities;
 
-    public Credentials(String username, String password, Set<Authority> authorities) {
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-    }
+  public Credentials(String username, String password, Set<Authority> authorities) {
+    this.username = username;
+    this.password = password;
+    this.authorities = authorities;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public Set<Authority> getAuthorities() {
-        return authorities;
-    }
+  public Set<Authority> getAuthorities() {
+    return authorities;
+  }
 
-    public void setAuthorities(Set<Authority> authorities) {
-        this.authorities = authorities;
-    }
+  public void setAuthorities(Set<Authority> authorities) {
+    this.authorities = authorities;
+  }
 }
