9f51cd70acc0e03da55c84569896fff877c80804
Luca Zaninotto <lucazanny@gmail.com>
2021-03-17 15:11:35 +0100

    moved CredentialsWithAuthoritiest to lombok

--- src/main/java/it/sweven/blockcovid/dto/CredentialsWithAuthorities.java
+++ src/main/java/it/sweven/blockcovid/dto/CredentialsWithAuthorities.java
@@ -2,26 +2,12 @@ package it.sweven.blockcovid.dto;
 
 import it.sweven.blockcovid.entities.user.Authority;
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class CredentialsWithAuthorities {
   private String username, password;
   private Set<Authority> authorities;
-
-  public CredentialsWithAuthorities(String username, String password, Set<Authority> authorities) {
-    this.username = username;
-    this.password = password;
-    this.authorities = authorities;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public Set<Authority> getAuthorities() {
-    return authorities;
-  }
 }
