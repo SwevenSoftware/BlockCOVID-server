7ac7ae9373b7185fa599f8467ce7b1fd18cd89c6
nR3D <nR3D@pm.me>
2021-03-21 16:05:20 +0100

    RoomAssembler.toCollectionModel test

--- src/test/java/it/sweven/blockcovid/assemblers/RoomAssemblerTest.java
+++ src/test/java/it/sweven/blockcovid/assemblers/RoomAssemblerTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 import it.sweven.blockcovid.entities.room.Room;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.EntityModel;
 
 class RoomAssemblerTest {
   private RoomAssembler assembler;
@@ -19,5 +22,15 @@ class RoomAssemblerTest {
   void toModel() {
     Room fakeRoom = mock(Room.class);
     assertEquals(fakeRoom, assembler.toModel(fakeRoom).getContent());
+  }
+
+  @Test
+  void toCollectionModel() {
+    List<Room> rooms = List.of(mock(Room.class), mock(Room.class), mock(Room.class));
+    assertEquals(
+        rooms,
+        assembler.toCollectionModel(rooms).getContent().stream()
+            .map(EntityModel::getContent)
+            .collect(Collectors.toList()));
   }
 }
