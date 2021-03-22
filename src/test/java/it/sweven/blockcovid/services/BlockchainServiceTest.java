52ae5015d7c0eafead6bf518ddf02ae615c24086
Luca Zaninotto <lucazanny@gmail.com>
2021-03-22 11:56:00 +0100

    test and unit test report should ignore configurations/generated code

--- src/test/java/it/sweven/blockcovid/services/BlockchainServiceTest.java
+++ src/test/java/it/sweven/blockcovid/services/BlockchainServiceTest.java
@@ -1,12 +1,17 @@
 package it.sweven.blockcovid.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.web3j.crypto.Credentials;
 import org.web3j.document.Document;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteFunctionCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.gas.ContractGasProvider;
 
 class BlockchainServiceTest {
@@ -26,5 +31,19 @@ class BlockchainServiceTest {
   }
 
   @Test
-  void runTest() {}
+  void runTest_validRun() throws Exception {
+    TransactionReceipt fakeReceipt = mock(TransactionReceipt.class);
+    RemoteFunctionCall<TransactionReceipt> fakeCall = mock(RemoteFunctionCall.class);
+    when(contract.add(any())).thenReturn(fakeCall);
+    when(fakeCall.send()).thenReturn(fakeReceipt);
+    assertDoesNotThrow(() -> service.run());
+  }
+
+  @Test
+  void runTest_failedCall() throws Exception {
+    RemoteFunctionCall<TransactionReceipt> fakeCall = mock(RemoteFunctionCall.class);
+    when(contract.add(any())).thenReturn(fakeCall);
+    when(fakeCall.send()).thenThrow(new Exception());
+    assertThrows(Exception.class, () -> service.run());
+  }
 }
