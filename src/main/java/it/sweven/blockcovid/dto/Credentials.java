e2ba7e23d0cdf7817b374f01f70b1a4fd5f8e06a
nR3D <nR3D@pm.me>
2021-03-26 20:37:06 +0100

    Validation checks on credentials

--- src/main/java/it/sweven/blockcovid/dto/Credentials.java
+++ src/main/java/it/sweven/blockcovid/dto/Credentials.java
@@ -3,16 +3,25 @@ package it.sweven.blockcovid.dto;
 import it.sweven.blockcovid.entities.user.Authority;
 import java.util.Set;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class Credentials {
-  @NotNull private final String username;
-  @NotNull private final String password;
+  @NotNull
+  @Size(min = 4, max = 16)
+  @Pattern(regexp = "^[a-zA-Z0-9]*$")
+  private final String username;
 
-  public CredentialsWithAuthorities withAuthorities(Set<Authority> authorities) {
+  @NotNull
+  @Size(min = 8)
+  private final String password;
+
+  public CredentialsWithAuthorities withAuthorities(
+      @NotNull @Size(min = 1) Set<Authority> authorities) {
     return new CredentialsWithAuthorities(this.username, this.password, authorities);
   }
 }
