6c39037127c914026ff1501f63bad183891b776e
badadumTss <lucazanny@gmail.com>
2021-04-03 11:11:29 +0200

    connections require now https

--- src/main/java/it/sweven/blockcovid/configurations/WebSecurityConfiguration.java
+++ src/main/java/it/sweven/blockcovid/configurations/WebSecurityConfiguration.java
@@ -7,7 +7,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 import it.sweven.blockcovid.security.TokenAuthenticationFilter;
 import it.sweven.blockcovid.security.TokenAuthenticationProvider;
 import it.sweven.blockcovid.services.TokenService;
-import it.sweven.blockcovid.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +17,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -38,23 +36,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
           new NegatedRequestMatcher(PROTECTED_URLS), new AntPathRequestMatcher("/api/login"));
 
   private final TokenAuthenticationProvider authenticationProvider;
-  private final UserService userService;
   private final TokenService tokenService;
-  private final PasswordEncoder passwordEncoder;
-  private final TokenAuthenticationProvider tokenProvider;
 
   @Autowired
   WebSecurityConfiguration(
-      TokenAuthenticationProvider authenticationProvider,
-      UserService userService,
-      TokenService tokenService,
-      PasswordEncoder passwordEncoder,
-      TokenAuthenticationProvider tokenProvider) {
+      TokenAuthenticationProvider authenticationProvider, TokenService tokenService) {
     this.authenticationProvider = authenticationProvider;
-    this.userService = userService;
     this.tokenService = tokenService;
-    this.passwordEncoder = passwordEncoder;
-    this.tokenProvider = tokenProvider;
   }
 
   @Override
