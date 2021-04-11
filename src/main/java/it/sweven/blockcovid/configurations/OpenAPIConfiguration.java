72dc6c65d19f7ad4be0b5f3d30e8f54722c7c092
nR3D <nR3D@pm.me>
2021-04-11 16:37:16 +0200

    spotlessApply

--- src/main/java/it/sweven/blockcovid/configurations/OpenAPIConfiguration.java
+++ src/main/java/it/sweven/blockcovid/configurations/OpenAPIConfiguration.java
@@ -3,18 +3,10 @@ package it.sweven.blockcovid.configurations;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "bearer",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer"
-)
-@OpenAPIDefinition(
-        info = @Info(title = "BlockCOVID")
-)
-public class OpenAPIConfiguration {
-}
+@SecurityScheme(name = "bearer", type = SecuritySchemeType.HTTP, scheme = "bearer")
+@OpenAPIDefinition(info = @Info(title = "BlockCOVID"))
+public class OpenAPIConfiguration {}
--- src/main/java/it/sweven/blockcovid/rooms/controllers/RoomsController.java
+++ src/main/java/it/sweven/blockcovid/rooms/controllers/RoomsController.java
@@ -1,6 +1,5 @@
 package it.sweven.blockcovid.rooms.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
