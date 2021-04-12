54ebec1fed756231bb0cd4a64792617a79c1a18f
nR3D <nR3D@pm.me>
2021-04-12 12:09:38 +0200

    New DTO for desk creation

---
+++ src/main/java/it/sweven/blockcovid/rooms/dto/NewDeskInfo.java
@@ -0,0 +1,10 @@
+package it.sweven.blockcovid.rooms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class NewDeskInfo {
+  private final Integer x, y;
+}
