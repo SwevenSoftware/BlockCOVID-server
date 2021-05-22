6c8a4c023669d4b083d9886c88f58885fbbfa208
badadumTss <lucazanny@gmail.com>
2021-05-22 10:08:19 +0200

    Provided a list of deskIds the admin can delete the valid ones

--- src/main/java/it/sweven/blockcovid/rooms/controllers/DeleteDeskController.java
+++ src/main/java/it/sweven/blockcovid/rooms/controllers/DeleteDeskController.java
@@ -6,23 +6,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.sweven.blockcovid.rooms.assemblers.DeskAssembler;
-import it.sweven.blockcovid.rooms.dto.DeskInfo;
 import it.sweven.blockcovid.rooms.dto.DeskWithRoomName;
 import it.sweven.blockcovid.rooms.entities.Desk;
+import it.sweven.blockcovid.rooms.exceptions.DeskNotFoundException;
 import it.sweven.blockcovid.rooms.services.DeskService;
 import it.sweven.blockcovid.users.entities.User;
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class DeleteDeskController implements RoomsController {
@@ -36,32 +35,45 @@ public class DeleteDeskController implements RoomsController {
     this.deskAssembler = deskAssembler;
   }
 
-  @DeleteMapping("/{roomName}/desks")
+  @DeleteMapping("/desks")
   @ApiResponses({
-    @ApiResponse(responseCode = "200", description = "Desk successfully deleted"),
-    @ApiResponse(
-        responseCode = "403",
-        description = "Method not allowed",
-        content = @Content(schema = @Schema(implementation = void.class))),
-    @ApiResponse(
-        responseCode = "404",
-        description = "Room or desk not found",
-        content = @Content(schema = @Schema(implementation = void.class)))
+          @ApiResponse(responseCode = "200", description = "Desk successfully deleted"),
+          @ApiResponse(
+                  responseCode = "403",
+                  description = "Method not allowed",
+                  content = @Content(schema = @Schema(implementation = void.class))),
+          @ApiResponse(
+                  responseCode = "404",
+                  description = "Room or desk not found",
+                  content = @Content(schema = @Schema(implementation = void.class)))
   })
   @PreAuthorize("#submitter.isEnabled() and #submitter.isAdmin()")
-  public EntityModel<DeskWithRoomName> delete(
-      @Parameter(hidden = true) @AuthenticationPrincipal User submitter,
-      @PathVariable String roomName,
-      @NotNull @RequestBody DeskInfo toDelete) {
-    try {
-      String idDesk = deskService.getDeskByInfoAndRoomName(toDelete, roomName).getId();
-      Desk deleted = deskService.deleteDeskById(idDesk);
-      DeskWithRoomName toReturn =
-          new DeskWithRoomName(
-              roomName, deleted.getId(), deleted.getX(), deleted.getY(), deleted.getDeskStatus());
-      return deskAssembler.toModel(toReturn);
-    } catch (NoSuchElementException exception) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
+  public CollectionModel<EntityModel<DeskWithRoomName>> delete(
+          @Parameter(hidden = true) @AuthenticationPrincipal User submitter,
+          @NotNull @RequestBody List<String> deskIds) {
+    List<DeskWithRoomName> deletedDesks =
+            deskIds.stream()
+                    .filter(
+                            id -> {
+                              try {
+                                deskService.getDeskById(id);
+                                return true;
+                              } catch (DeskNotFoundException e) {
+                                return false;
+                              }
+                            })
+                    .map(
+                            grantedId -> {
+                              String roomName = deskService.getRoom(grantedId).getName();
+                              Desk deleted = deskService.deleteDeskById(grantedId);
+                              return new DeskWithRoomName(
+                                      roomName,
+                                      deleted.getId(),
+                                      deleted.getX(),
+                                      deleted.getY(),
+                                      deleted.getDeskStatus());
+                            })
+                    .collect(Collectors.toList());
+    return deskAssembler.toCollectionModel(deletedDesks);
   }
 }
