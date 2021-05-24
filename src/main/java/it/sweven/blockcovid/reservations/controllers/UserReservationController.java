ce44917e242598237ec7c46f226f539373445706
badadumTss <lucazanny@gmail.com>
2021-05-24 10:20:14 +0200

    added swagger annotations

--- src/main/java/it/sweven/blockcovid/reservations/controllers/UserReservationController.java
+++ src/main/java/it/sweven/blockcovid/reservations/controllers/UserReservationController.java
@@ -1,6 +1,10 @@
 package it.sweven.blockcovid.reservations.controllers;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.sweven.blockcovid.reservations.assemblers.ReservationWithRoomAssembler;
 import it.sweven.blockcovid.reservations.dto.ReservationWithRoom;
 import it.sweven.blockcovid.reservations.servicies.ReservationService;
@@ -8,14 +12,12 @@ import it.sweven.blockcovid.users.entities.User;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserReservationController implements ReservationController {
@@ -31,11 +33,25 @@ public class UserReservationController implements ReservationController {
   }
 
   @GetMapping("{username}")
+  @ApiResponses({
+    @ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved all reservation between 'from' and 'to' (included)"),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Time incorrectly formatted",
+        content = @Content(schema = @Schema(implementation = void.class))),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Method not allowed",
+        content = @Content(schema = @Schema(implementation = void.class)))
+  })
   @PreAuthorize("#submitter.isAdmin()")
+  @ResponseBody
   public CollectionModel<EntityModel<ReservationWithRoom>> userReservations(
       @Parameter(hidden = true) @AuthenticationPrincipal User submitter,
-      @RequestParam LocalDateTime from,
-      @RequestParam LocalDateTime to,
+      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to,
       @PathVariable String username) {
     return reservationWithRoomAssembler.toCollectionModel(
         reservationService.findByTimeInterval(from, to).stream()
